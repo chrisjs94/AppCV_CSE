d5ed2e360cdede483c5366db1b69e2b74f4621bb
Christian Silva <christiamsilva758@gmail.com>
2022-01-26 08:30:24 -0600

    added functionality of search in padron

---
+++ APPCV_CSE/Controllers/SearchController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Web.Mvc;
+using APPCV_CSE.Datos;
+using APPCV_CSE.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace APPCV_CSE.Controllers
+{
+    public class SearchController : Controller
+    {
+        private DBJUNTASEntities db;
+        public SearchController()
+        {
+            db = new DBJUNTASEntities();
+        }
+
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Index(BusquedaPersona data)
+        {
+            db.Database.CommandTimeout = 500;
+            if (data == null)
+            {
+                throw new ArgumentNullException("data object is null");
+            }
+            else
+            {
+                if (!String.IsNullOrEmpty(data.txtSearch))
+                {
+                    if (data.radioBuscarPor == "1") //Cedula de identidad
+                    {
+                        var qryporCedula = db.PADRON.Where(cond => cond.ci.ToUpper() == data.txtSearch.ToUpper()).ToList();
+                        return Json(qryporCedula, JsonRequestBehavior.AllowGet);
+                    }
+                    else if (data.radioBuscarPor == "2") //Nombre y/o apellido
+                    {
+                        var qryporNombre = db.PADRON.Where(cond =>
+                        new string[]
+                        {
+                            (cond.pnom + cond.snom + cond.papell + cond.sappell).Replace(" ", "").ToUpper(),
+                            (cond.papell + cond.sappell + cond.pnom + cond.snom).Replace(" ", "").ToUpper(),
+                            (cond.papell + cond.sappell + cond.pnom).Replace(" ", "").ToUpper(),
+                            (cond.pnom + cond.papell).Replace(" ", "").ToUpper(),
+                            (cond.papell + cond.sappell).Replace(" ", "").ToUpper(),
+                            (cond.papell + cond.pnom).Replace(" ", "").ToUpper(),
+                            (cond.papell + cond.snom).Replace(" ", "").ToUpper(),
+                        }.Any(a => a.Contains(data.txtSearch.ToUpper().Replace(" ", ""))));
+
+                        return Json(qryporNombre, JsonRequestBehavior.AllowGet);
+                    }
+                    else
+                        return Json(new List<PADRON>(), JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    throw new ArgumentNullException("data.txtSearch is null");
+                }
+            }
+        }
+    }
+}
---
+++ APPCV_CSE/Models/BusquedaPersona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APPCV_CSE.Models
+{
+    public class BusquedaPersona
+    {
+        public string txtSearch { get; set; }
+        public string radioBuscarPor { get; set; }
+    }
+}
